bd1d33c05455ae88348631556849f930da7a3578
myungjun12 <nenncmj@naver.com>
2024-07-05 14:30:25 +0900

    "react"

---
+++ my-app/src/component/R16_Context4/작성공간.js
@@ -0,0 +1,46 @@
+import React, {useState, useContext} from "react";
+
+import 유저정보모두저장 from './유저정보모두저장.js';
+
+// input 타입을 만들고 저장하는 버튼을 생성
+const 작성하는공간 = () => {
+
+    const {userList, setUserList} = useContext(유저정보모두저장);
+
+    const [inputName, setInputName] = useState('');
+    const [inputPhone, setInputPhone] = useState('');
+    //영어는 중괄호 생략 가능
+    const 유저추가 = () => {
+        
+        const user = {"inputName"  :  inputName,
+                      "inputPhone" :  inputPhone}
+        
+        const newUserList = [...userList, user];
+
+        setUserList(newUserList);
+
+        //멤버가 저장이 되면 input에 작성된 내용 모두 지우기
+        setInputName(''); // 이름칸 모두 지우기
+        setInputPhone('');// 번호칸 모두 지우기
+    }
+
+    return (
+        <div>
+            <label htmlFor="inputName">이름</label>
+            <input type="text"
+                   id="inputName"
+                   onChange={ (e) => {setInputName(e.target.value)}}
+                   value={inputName} />
+        <br />
+        
+        <label htmlFor="inputPhone">번호</label>
+        <input type="text"
+               id="inputPhone"
+               onChange={ (e) => {setInputPhone(e.target.value)}}
+               value={inputPhone} />
+
+        <button onClick={유저추가}>멤버추가</button>
+        </div>
+    )
+}
+export default 작성하는공간;
